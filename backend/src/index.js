75e0b2a3d644c5954570b5d13312a167bede1c86
Tales Mantovani <talesmantovani100@gmail.com>
2020-03-27 14:16:46 -0300

    update the main file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const cors = require('cors')
+// const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(routes);
 
