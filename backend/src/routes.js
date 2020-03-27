c565f2d7eaf8445d5f55f8a6dc7c700a469d123b
Tales Mantovani <talesmantovani100@gmail.com>
2020-03-27 14:16:22 -0300

    update the routes file

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,22 @@
 const routes = require('express').Router()
 const OngController = require('./controllers/OngsController')
+const IncidentsController = require("./controllers/IncidentsController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
+//ONGS
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+//INCIDENTS
+routes.get("/incidents", IncidentsController.index);
+routes.post("/incidents", IncidentsController.create);
+routes.delete("/incidents/:id", IncidentsController.delete);
+
+//PROFILE
+routes.get("/profile", ProfileController.index);
+
+//LOGIN
+routes.post("/sessions", SessionController.create);
 
 module.exports = routes;
